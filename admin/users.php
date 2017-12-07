e87a6eb72614e79ebf1874cf0e6960f1d3772799
mattaxell <hello@mattaxell.com>
2017-12-07 18:01:05 +0000

    Current user role function

--- admin/users.php
+++ admin/users.php
@@ -1,5 +1,20 @@
 <?php
 
+/*
+ * Get user's role
+ *
+ * If $user parameter is not provided, returns the current user's role.
+ * Only returns the user's first role, even if they have more than one.
+ * Returns false on failure.
+ *
+ * @param  mixed       $user User ID or object.
+ * @return string|bool       The User's role, or false on failure.
+ */
+function get_user_role($user = null) {
+	$user = $user ? new WP_User($user) : wp_get_current_user();
+	return $user->roles ? $user->roles[0] : 'guest';
+}
+
 /**
  * Conditional check for the current user role
  * @param  array $roles List of roles to check
