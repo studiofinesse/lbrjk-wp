daed5ceb7d282a99676e255a0b06f63a1aef5678
mattaxell <hello@mattaxell.com>
2019-05-09 15:08:47 +0100

    Extend pagination function to make it more flexible

--- public/pagination.php
+++ public/pagination.php
@@ -8,7 +8,15 @@ if ( ! defined( 'ABSPATH') ) exit;
  * @param  string $class Class for wrapping div and BEM prefix for inner elements
  * @return string        Simple markup
  */
-function lj_prev_next_pagination( $class = 'post-pagination' ) {
+function lj_prev_next_pagination( $args = null ) {
+
+	$defaults = array(
+		'wrapper_class' => 'post-pagination',
+		'prev_arrow' => '',
+		'next_arrow' => '',
+	);
+
+	$args = wp_parse_args( $args, $defaults );
 
 	global $paged;
 	global $wp_query;
@@ -22,9 +30,32 @@ function lj_prev_next_pagination( $class = 'post-pagination' ) {
 	$next_text = 'Older Posts';
 
 	if( $pages > 1 ) {
-		echo '<div class="' . $class . '">';
-		echo $has_prev ? '<a href="' . $prev_link . '" class="prev">' . $prev_text . '</a>' : '<span class="prev disabled">' . $prev_text . '</span>';
-		echo $has_next ? '<a href="' . $next_link . '" class="next">' . $next_text . '</a>' : '<span class="next disabled">' . $next_text . '</span>';
+		echo '<div class="' . $args['wrapper_class'] . '">';
+
+		if( $has_prev ) {
+			echo '<a href="' . $prev_link . '">';
+			echo $args['prev_arrow'];
+			echo '<span>' . $prev_text . '</span>';
+			echo '</a>';
+		} else {
+			echo '<span class="disabled">';
+			echo $args['prev_arrow'];
+			echo '<span>' . $prev_text . '</span>';
+			echo '</span>';
+		}
+
+		if( $has_next ) {
+			echo '<a href="' . $next_link . '">';
+			echo '<span>' . $next_text . '</span>';
+			echo $args['next_arrow'];
+			echo '</a>';
+		} else {
+			echo '<span class="disabled">';
+			echo '<span>' . $next_text . '</span>';
+			echo $args['next_arrow'];
+			echo '</span>';
+		}
+
 		echo '</div>';
 	}
 
